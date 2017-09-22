1864d8dedccf8850016ffabf46372cbd6d8997e6
JianGe <25741915@qq.com>
2017-09-22 15:09:40 +0800

    change Test package

--- js/Test/TestArrayList_Object.js
+++ js/Test/TestArrayList_Object.js
@@ -4,27 +4,27 @@ const PERSON = require('../DataStructure/Person.js');
 
 let list = new LIST.ArrayList();
 let log = new LOG.Log();
-let Person = require('../DataStructure/Person.js').Person;
+let Person = PERSON.Person;
 console.log();
 
-
 for (let idx = 0; idx < 50; idx++) {
-    let person = new Person("name_" + idx, "age_" + idx);
+    let person = new Person("name_" + idx, idx);
 
     list.append(person);
 }
 
-let person1 = new Person("name_1", "age_1");
+let person1 = new Person("name_1", 1);
 let person2 = new Person();
 
 log.info("list contains person1 is: " + list.contains(person1));
 log.info("list contains person2 is: " + list.contains(person2));
+console.log();
+
 log.info("the position of person1 in list is: " + list.find(person1));
-log.info("the position of person1 in list is: " + list.find(person2));
+log.info("the position of person2 in list is: " + list.find(person2));
 console.log();
 
 for (list.front(); list.currPos() < list.length(); list.next()) {
-
     log.info(list.getElement(), "position is: " + list.pos, "total length: " + list.length());
 }
 
--- js/Test/TestArrayList_Simple.js
+++ js/Test/TestArrayList_Simple.js
@@ -1,27 +1,26 @@
 const LIST = require('../DataStructure/ArrayList.js');
+const LOG = require('../util.js');
 
-var list = new LIST.ArrayList();
-
+let list = new LIST.ArrayList();
+let log = new LOG.Log();
+console.log();
 
 for (let idx = 0; idx < 50; idx++) {
     list.append("aa" + idx);
 }
 
-var logger = require('tracer').console();
-
-// logger.Log(list.insert("fuck", "aa1"));
-logger.Log(list.dataStore);
-logger.Log(list.contains("aa1"));
-logger.Log(list.find("aa0"));
-
+log.info("Elements in list are: " + list.dataStore);
+log.info("This list contains \"aa1\" is: " + list.contains("aa1"));
+log.info("This list contains \"aa100\" is: " + list.contains("aa100"));
+log.info("The position of \"aa0\" is: " + list.find("aa0"));
+console.log();
 
 for (list.front(); list.currPos() < list.length(); list.next()) {
-
-    console.log(list.getElement(), " , ", list.pos, " , ", list.length());
+    log.info(list.getElement(), list.pos, list.length());
 }
+console.log();
 
-for (list.end(); list.currPos() >=0; list.prev()) {
-
-    console.log(list.getElement(), " , ", list.pos, " , ", list.length());
+for (list.end(); list.currPos() >= 0; list.prev()) {
+    log.info(list.getElement(), list.pos, list.length());
 }
 
